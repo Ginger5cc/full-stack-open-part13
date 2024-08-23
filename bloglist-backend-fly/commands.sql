a72b1c4e1c149cba5c8d066bec0332cb611c0de0
Ginger <gingerginger5cc@gmail.com>
2024-08-23 00:29:35 -0400

    ex13.1-13.3 - connect to postgres by fly.io and create table

---
+++ bloglist-backend-fly/commands.sql
@@ -0,0 +1,17 @@
+flyctl postgres connect -a bloglistbackendpostgres
+postgres=# \d
+
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer DEFAULT 0
+);
+
+Dan Abramov: 'On let vs const', 0 likes
+Laurenz Albe: 'Gaps in sequences in PostgreSQL', 0 likes
+insert into blogs (author, url, title, likes) values ('Dan Abramov', 'www.dan.com', 'On let vs const', 0);
+insert into blogs (author, url, title, likes) values ('Laurenz Albe', 'www.laurenz.com', 'Gaps in sequences in PostgreSQL', 0);
+insert into blogs (author, url, title, likes) values ('Henry Wu', 'www.happy.com', 'Merry Christmas', 6);
+insert into blogs (author, url, title, likes) values ('Billie', 'www.billie.com', 'Happy Birthday', 17);
