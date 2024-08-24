42ecfe3cd5dd50cbe71ef8e65113e4461404a544
Ginger <gingerginger5cc@gmail.com>
2024-08-23 22:10:23 -0400

    ex13.13 - implement filering by keywords for the app

--- bloglist-backend-fly/controllers/blogs.js
+++ bloglist-backend-fly/controllers/blogs.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const router = require('express').Router()
 const { SECRET } = require('../util/config')
 const { Blog, User } = require('../models')
+const { Op } = require("sequelize");
 
 const blogFinder = async(req, res, next) => {
   req.blog =  await Blog.findByPk(req.params.id)
@@ -9,12 +10,31 @@ const blogFinder = async(req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  let where = {}
+
+  if (req.query.search) {
+    where = {
+      [Op.or]: [
+        {
+          title: {
+            [Op.iLike]:`%${req.query.search}%`
+          }
+        },
+        {
+          author: {
+            [Op.iLike]:`%${req.query.search}%`
+          }
+        }
+      ]
+    }
+  }
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
